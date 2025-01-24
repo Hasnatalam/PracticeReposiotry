596f9cf440c16919cd40b521dace39b8b0c4571b
Hasnat alam <hasnatalamofficial@gmail.com>
2025-01-24 18:15:44 +0530

    change 111

--- GitHubPractice/src/main/java/com/hasnat/GitHubPractice/App.java
+++ GitHubPractice/src/main/java/com/hasnat/GitHubPractice/App.java
@@ -3,7 +3,7 @@ package com.hasnat.GitHubPractice;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");//789
+        System.out.println("Hello World");//111
 
         System.out.println("Print by Dev");
     }
