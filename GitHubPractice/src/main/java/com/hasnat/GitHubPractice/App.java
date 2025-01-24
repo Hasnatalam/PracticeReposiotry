50b65477c2778c53c96bc4f7cdce5349387b505f
Hasnat alam <hasnatalamofficial@gmail.com>
2025-01-24 18:00:01 +0530

    adding one line

--- GitHubPractice/src/main/java/com/hasnat/GitHubPractice/App.java
+++ GitHubPractice/src/main/java/com/hasnat/GitHubPractice/App.java
@@ -3,5 +3,6 @@ package com.hasnat.GitHubPractice;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Print by Dev");
     }
 }
