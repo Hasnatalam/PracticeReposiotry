55d4cf44aa406fbeb4af19f830f0a2ee02dc1460
Hasnat alam <hasnatalamofficial@gmail.com>
2025-01-26 21:55:54 +0530

    adding one line for stash checking

--- GitHubPractice/src/main/java/com/hasnat/branch1/Branch1.java
+++ GitHubPractice/src/main/java/com/hasnat/branch1/Branch1.java
@@ -4,5 +4,6 @@ public class Branch1 {
 	public static void main(String[] args) {
 		System.out.println("Branch1");
 		System.out.println("Added by dev");
+		System.out.println("adding one line by dev");
 	}
 }
